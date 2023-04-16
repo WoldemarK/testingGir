684d4ce15c273f183dd5898eacff5ae9f0dc037c
WoldemarK <kovtynov.vladimir@gmail.com>
2023-04-17 01:20:00 +0300

    p

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-       git
+       int []a = {89, 57, 91, 47, 95, 3, 27, 22, 67, 99};
+        binarySearch(a,67);
+    }
+    public static int binarySearch(int arr[], int elementToSearch) {
+
+        int firstIndex = 0;
+        int lastIndex = arr.length - 1;
+
+        while(firstIndex <= lastIndex) {
+            int middleIndex = (firstIndex + lastIndex) / 2;
+
+            if (arr[middleIndex] == elementToSearch) {
+                return middleIndex;
+            }
+
+            else if (arr[middleIndex] < elementToSearch)
+                firstIndex = middleIndex + 1;
+
+            else if (arr[middleIndex] > elementToSearch)
+                lastIndex = middleIndex - 1;
+
+        }
+        return -1;
     }
 }
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,8 +1,40 @@
 package org.example;
-
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello add-feature2EEND");
+        int array[] = {5, 9, 4, 6, 5, 3};
+        quickSort(array, 0, 5);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
     }
 }
